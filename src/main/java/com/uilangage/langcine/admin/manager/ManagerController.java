533bd2df00b64644af5353e5ff7ec800c110dd32
강남\82103 <kangng0806@gmail.com>
2023-10-23 10:50:34 +0900

    [강남규] 관리자 로그인

---
+++ src/main/java/com/uilangage/langcine/admin/manager/ManagerController.java
@@ -0,0 +1,8 @@
+package com.uilangage.langcine.admin.manager;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ManagerController {
+
+}
---
+++ src/main/java/com/uilangage/langcine/admin/manager/ManagerRestController.java
@@ -0,0 +1,8 @@
+package com.uilangage.langcine.admin.manager;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ManagerRestController {
+
+}
