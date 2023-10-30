0d03f0a2f4bad0b402b1d3f85e3086a444ddb52c
강남\82103 <kangng0806@gmail.com>
2023-10-30 12:44:06 +0900

    [강남규] 누락추가 #3

--- src/main/java/com/uilangage/langcine/admin/movie/dto/ScreeningDO.java
+++ src/main/java/com/uilangage/langcine/admin/movie/dto/ScreeningDO.java
@@ -1,5 +1,14 @@
 package com.uilangage.langcine.admin.movie.dto;
 
+import java.sql.Date;
+
 public class ScreeningDO {
 
+	private int id;
+	private int theaterId;
+	private int movieId;
+	private int runningTime;
+	private Date movieTime;
+	
+	
 }
--- src/main/java/com/uilangage/langcine/common/CalculateTime.java
+++ src/main/java/com/uilangage/langcine/common/CalculateTime.java
@@ -1,5 +1,11 @@
 package com.uilangage.langcine.common;
 
 public class CalculateTime {
-
+	
+//	입력받은 영화 id로 러닝타임을 불러온다
+//	상영관은 9시부터 새벽 2시까지로 지정한다
+//	청소시간을 10분으로 잡고 러닝타임 + 10분
+	
+	
+	
 }
