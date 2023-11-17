751f84c95757b0efa506b04d078e5fed3bff37cd
DESKTOP-PL3OFLT\user <kangng0806@gmail.com>
2023-11-17 18:19:17 +0900

    [강남규] 영화 리스트 선별 조건 수정

--- src/main/java/com/uilangage/langcine/admin/movie/repository/MovieRepository.java
+++ src/main/java/com/uilangage/langcine/admin/movie/repository/MovieRepository.java
@@ -14,6 +14,10 @@ public interface MovieRepository {
 	
 	public Movie getMovieInfo(@Param("id") int id);
 	
+//	개봉일이 오늘날짜보다 뒤에 있을 때
+	public List<Movie> getMovieExpected();
+	
+	public List<Movie> getMovieByExposureScreen();
 	
 	public List<Movie> getMovie();	
 	
